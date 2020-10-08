059910f5fb7239de9e1b15a6353f233a8531a073
Michael Harrison <michael@ntechmedia.com>
2020-10-08 15:58:47 +1000

    Refactored Dummy::SchemaWithCustomRedisClient class use new ruby graphql setup

---
+++ spec/support/custom_schema.rb
@@ -0,0 +1,10 @@
+require_relative 'types/query_type'
+
+CUSTOM_REDIS_CLIENT = Redis.new
+
+class CustomSchema < GraphQL::Schema
+  query_analyzer GraphAttack::RateLimiter.new(
+    redis_client: CUSTOM_REDIS_CLIENT,
+    )
+  query Types::QueryType
+end
